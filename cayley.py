96f8a52ec5500ae29805ce339052020073729264
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-07-10 19:36:56 +1200

    Add limit plotting test

--- cayley.py
+++ cayley.py
@@ -17,17 +17,10 @@ def _list_to_tuple(function):
     return wrapper
 
 class GroupCache:
-    def __init__(self, generators, labels = None, inv_labels = None):
-        if labels == None:
-            labels = [f'x{n}' for n in range(1,len(generators) + 1)]
-        if inv_labels == None:
-            inv_labels = [l.swapcase() for l in labels]
-        if len(generators) != len(labels) or len(labels) != len(inv_labels):
-            raise IndexError("in GroupCache inconsistent lengths to constructor")
-        self.gen_to_inv = dict(zip(labels,inv_labels), **dict(zip(inv_labels,labels)))
-        self.generators = dict(zip(labels, generators))
-        self.inverses = dict(zip(inv_labels, [inv(g) for g in generators]))
-        self.all_of_them = dict(self.inverses, **self.generators)
+    def __init__(self, generators):
+        self.length = len(generators)
+        self.generators = generators + [inv(g) for g in generators]
+        self.gen_to_inv = [r for r in itertools.chain(range(self.length,2*self.length), range(0,self.length))]
 
     @_list_to_tuple
     @functools.cache
@@ -35,21 +28,18 @@ class GroupCache:
         if word == ():
             return np.identity(2)
         else:
-            return np.dot(self.all_of_them[word[0]], self[word[1:]])
-
-    def inverse_letter(self, let):
-        return self.gen_to_inv[let]
+            return np.dot(self.generators[word[0]], self[word[1:]])
 
     def __len__(self):
-        return len(generators)
+        return self.length
 
     def free_cayley_graph_locally(self, word):
         if word == []:
-            yield from [[x] for x in self.all_of_them]
+            yield from [[w] for w in range(2*self.length)]
         else:
-            for (lab,gen) in self.all_of_them.items():
-                if lab != self.inverse_letter(word[-1]):
-                    yield word + [lab]
+            for lab in range(2*self.length):
+                if lab != self.gen_to_inv[word[0]]:
+                    yield [lab] + word
 
     def free_cayley_graph_bfs(self, depth):
         last_list = [[]]
@@ -62,10 +52,3 @@ class GroupCache:
             last_list = this_list
 
 
-G = GroupCache([np.array([[1,1],[0,1]]), np.array([[1,0],[4j,1]])])
-def testfn():
-    for w in G.free_cayley_graph_bfs(15):
-        pass
-
-print(timeit.timeit(testfn, number=1))
-print(timeit.timeit(testfn, number=1))
---
+++ limits.py
@@ -0,0 +1,20 @@
+import cayley
+import numpy as np
+import holoviews as hv
+import pandas as pd
+hv.extension('bokeh')
+from bokeh.plotting import show
+
+
+def testfn():
+    G = cayley.GroupCache([np.array([[1,1],[0,1]]), np.array([[1,0],[2j,1]])])
+    L = []
+    base = np.array([[0],[1]])
+    for w in G.free_cayley_graph_bfs(8):
+        point = np.dot(G[w], base)
+        L.append(point[0]/point[1])
+    df = pd.DataFrame(data=[(float(np.real(point)), float(np.imag(point))) for point in L], columns=['x','y'], copy=False)
+    scatter = hv.Scatter(df, 'x','y')
+    show(hv.render(scatter.redim(x=hv.Dimension('x', range=(-4, 4)),y=hv.Dimension('y', range=(-4, 4)))))
+
+testfn()
