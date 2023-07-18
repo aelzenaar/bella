86e052fc92bed500261a131292f6a499360dc7ab
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-07-18 16:54:56 +1200

    Riley limit sets: display fixed points

--- limits.py
+++ limits.py
@@ -8,9 +8,13 @@ import panel as pn
 
 def limit_set_points(p=2,q=7,mure=2, muim=2, depth=15, logpoints=3):
     G = riley.RileyGroup(p, q, mure+muim*1j)
+    α = np.exp(np.pi*1j/p)
+    β = np.exp(np.pi*1j/q)
+    μ = mure + muim*1j
     df = G.coloured_limit_set_mc(depth,10**logpoints)
-    scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour']).opts(marker = "dot", size = 1,  color = hv.dim('colour'), width=800, height=800, data_aspect=1, cmap='Category10').redim(x=hv.Dimension('x', range=(-1.5, 1.5)),y=hv.Dimension('y', range=(-0.5, 2.0)))
-    return scatter
+    scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour']).opts(marker = "dot", size = 1,  color = 'black', width=800, height=800, data_aspect=1, cmap='Category10').redim(x=hv.Dimension('x', range=(-4,4)),y=hv.Dimension('y', range=(-4, 4)))
+    return scatter * hv.Points([[np.real(-α/(α**2-1)), np.imag(-α/(α**2-1))]]).opts(marker = "dot", size = 20,  color = 'red', width=800, height=800, data_aspect=1, cmap='Category10')\
+      * hv.Points([[0,0], [np.real((β - β**-1)/μ), np.imag((β - β**-1)/μ)]]).opts(marker = "dot", size = 20,  color = 'green', width=800, height=800, data_aspect=1, cmap='Category10')
 
 plot = hv.DynamicMap(limit_set_points, kdims=[hv.Dimension('p', values=[2,3,4,5,6,7,8,9,10,20,1000], default=2),
                                               hv.Dimension('q', values=[2,3,4,5,6,7,8,9,10,20,1000], default=3),
