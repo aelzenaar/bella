7aafc8049543b8e6e2b2317e44f32e74db7d32e8
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-07-21 10:02:35 +1200

    Fiddle with parameters of animator

--- examples/animate_slices.py
+++ examples/animate_slices.py
@@ -7,18 +7,18 @@ from bokeh.plotting import show
 from mpmath.libmp.libhyper import NoConvergence as BadThing
 
 θ = 0
-depth = 30
+depth = 40
 
-first = -2000
-last = 2000
-scale = 1000
+first = -200
+last = 200
+scale = 100
 
 def one_frame(kk):
     print(f"#{kk}")
     η = mp.exp(kk/scale)
     L = list(moduli.approximate_riley_slice(θ,η, depth))
     df = pd.DataFrame(data=[(float(point.real), float(point.imag)) for point in L], columns=['x','y'], copy=False)
-    scatter = hv.Scatter(df, 'x','y').opts(marker = "dot", size = 5, width=800, height=800, data_aspect=1, color='black') * hv.Text(0,3,f"η = exp({kk}/{scale}) = {complex(η):.2f}")
+    scatter = hv.Scatter(df, 'x','y').opts(marker = "dot", size = 5, width=800, height=800, data_aspect=1, color='black') * hv.Text(0,3,f"β = exp(i ({complex(η):.2f})) = {complex(mp.exp(1j*η)):.2f}")
     hv.save(scatter.redim(x=hv.Dimension('x', range=(-4.5,4.5)),y=hv.Dimension('y', range=(-4.5, 4.5))), f'animation/frame{-first+kk:05}.png', fmt='png')
     print(f"Done {kk}")
 
