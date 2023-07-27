7a1aa574a682ed133b9623ff7fcd19c064c4fbec
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-07-27 15:52:38 +1200

    Add level_set option when solving Farey polynomials

--- bella/moduli.py
+++ bella/moduli.py
@@ -5,7 +5,7 @@ from . import farey
 from mpmath import mp
 from numpy.polynomial import Polynomial
 
-def approximate_riley_slice(θ, η, depth=None, try_fast=True, maxsteps=500,extraprec=1000):
+def approximate_riley_slice(θ, η, depth=None, try_fast=True, maxsteps=500,extraprec=1000, level_set = -2):
     """ Generator yielding points of the Riley slice on generator angles θ and η.
 
         Parameters:
@@ -13,6 +13,7 @@ def approximate_riley_slice(θ, η, depth=None, try_fast=True, maxsteps=500,extr
         depth -- maximal denominator of Farey polynomial to use
         try_fast -- if True, do a rough calculation first making things significantly faster
         maxsteps, extraprec -- passed directly to mpmath.polyroots().
+        level_set -- level set of the Farey polynomials to compute (-2 for cusp points).
     """
     α = mp.exp(1j*θ)
     β = mp.exp(1j*η)
@@ -20,7 +21,7 @@ def approximate_riley_slice(θ, η, depth=None, try_fast=True, maxsteps=500,extr
     for (r,s) in farey.walk_tree_bfs(depth):
         # It is significantly faster (on the order of 20sec vs 110sec) to do a rough computation first using numpy
         # and then feed the result into mpmath's solver.
-        P = farey.farey_polynomial_numpy(r,s,α,β) + 2
+        P = farey.farey_polynomial_numpy(r,s,α,β) - level_set
         fast_roots = Polynomial([float(c) for c in P]).roots() if try_fast else None
         yield from mp.polyroots(list(reversed(P.coef)), maxsteps=maxsteps, extraprec=extraprec, roots_init=fast_roots)
 
--- examples/plotslice.py
+++ examples/plotslice.py
@@ -6,7 +6,7 @@ hv.extension('bokeh')
 from bokeh.plotting import show
 import panel as pn
 
-depth = 30
+depth = 40
 
 # Riley polynomials (red)
 riley_df = pd.DataFrame(data=[(float(point.real), float(point.imag)) for point in moduli.riley_polynomial_roots(depth)], columns=['x','y'])
@@ -16,6 +16,10 @@ riley_roots = hv.Scatter(riley_df, 'x','y').opts(marker = "dot", size = 4, width
 farey_df = pd.DataFrame(data=[(float(point.real), float(point.imag)) for point in moduli.approximate_riley_slice(0, 0, depth)], columns=['x','y'])
 farey_roots = hv.Scatter(farey_df, 'x','y').opts(marker = "dot", size = 4, width=800, height=800, data_aspect=1, color='blue')
 
-overlay = hv.NdOverlay({'Riley': riley_roots, 'Farey': farey_roots}).redim(x=hv.Dimension('x', range=(-4,4)),y=hv.Dimension('y', range=(-4, 4)))
+# Farey polynomial zeros (green)
+farey0_df = pd.DataFrame(data=[(float(point.real), float(point.imag)) for point in moduli.approximate_riley_slice(0, 0, depth, level_set = 0)], columns=['x','y'])
+farey0_roots = hv.Scatter(farey_df, 'x','y').opts(marker = "dot", size = 4, width=800, height=800, data_aspect=1, color='green')
+
+overlay = hv.NdOverlay({'Riley': riley_roots, 'Farey': farey_roots, 'Farey^-1(0)': farey0_roots}).redim(x=hv.Dimension('x', range=(-4,4)),y=hv.Dimension('y', range=(-4, 4)))
 
 pn.panel(overlay).servable(title='The Riley slice, approximated with different polynomials')
