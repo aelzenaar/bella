137d880cbf7b5f44a9d97163bab3364af32a91ea
Alex Elzenaar <albeetle@gmail.com>
2023-08-06 18:30:53 +1200

    Update animate_limits_as_slices_vary to get around memory leak
    There is a memory leak in holoviews. We now run every slice computation
    in a different process, and rasterise with datashader to reduce memory
    usage further.

--- examples.old/animate_limits_as_slices_vary.py
+++ examples.old/animate_limits_as_slices_vary.py
@@ -3,28 +3,27 @@ import mpmath as mp
 import holoviews as hv
 import pandas as pd
 hv.extension('bokeh')
-from bokeh.plotting import show
 from mpmath.libmp.libhyper import NoConvergence as BadThing
+import multiprocessing
 
-θ = 0
-depth = 20
-logpoints = 4
-
-first = 0
-last = 200
-scale = 100
-
-def one_frame(kk):
+def one_frame(kk,θ,depth,logpoints,first,scale):
     print(f"#{kk}")
     η = (kk/scale) * mp.pi
     G = riley.RileyGroup(θ,η,4j)
     df = G.coloured_limit_set_mc(depth,10**logpoints)
-    scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour']).opts(marker = "dot", size = 1,  color = 'colour', width=800, height=800, data_aspect=1, cmap='Category10')
+    scatter = hv.rasterize(hv.Scatter(df, kdims = ['x'], vdims = ['y','colour']).opts(marker = "dot", size = 1,  color = 'colour', width=800, height=800, data_aspect=1, cmap='Category10'))
     hv.save(scatter.redim(x=hv.Dimension('x', range=(-2,2)),y=hv.Dimension('y', range=(-2, 2))), f'animation/frame{-first+kk:05}.png', fmt='png')
     print(f"Done {kk}")
 
-for kk in range(first, last):
-    try:
-        one_frame(kk)
-    except BadThing:
-        print(f"**BAD {kk}")
+if __name__=='__main__':
+    θ = 0
+    depth = 10
+    logpoints = 4
+
+    first = 0
+    last = 200
+    scale = 100
+    
+    with multiprocessing.Pool(4, maxtasksperchild=1) as pool:
+        pool.starmap(one_frame, [ (kk,θ,depth,logpoints,first,scale) for kk in range(first, last) ], chunksize=1)
+
