78b27288b07922038d8019dc54cbcd14f547c2c6
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-08-09 22:50:17 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# only for compatibility with old python and pip
+from setuptools import setup
+
+setup(dependencies = [
+    "holoviews",
+    "bokeh",
+    "pandas",
+    "panel",
+    "pyadic",
+    "mpmath"
+],
+name = "bella",
+version = "0.0.99"
+)
