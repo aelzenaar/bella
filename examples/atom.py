7f46999f2b78c2de97f3d9a49bb85cb30816dc6d
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-08-11 13:20:38 +1200

    Update atom.py to use datashader

--- examples/atom.py
+++ examples/atom.py
@@ -11,12 +11,11 @@ hv.extension('bokeh')
 from mpmath import mp
 from functools import reduce
 import panel as pn
-
-panel_app = __name__.startswith('bokeh')
-if panel_app:
-    print("    atom.py is a panel app")
-else:
-    print("    atom.py producing image")
+from holoviews.operation.datashader import datashade, ResampleOperation2D
+# https://stackoverflow.com/a/74145904/17047536
+ResampleOperation2D.width=2000
+ResampleOperation2D.height=2000
+ResampleOperation2D.pixel_ratio=2
 
 class AtomGroup(cayley.GroupCache):
     def __init__(self, n, M, speedfactor = 0.5):
@@ -45,30 +44,35 @@ class AtomGroup(cayley.GroupCache):
 
         super().__init__(cayley.generators_from_circle_inversions(self.circles1+self.circles2, []))
 
-num_points = 10**6
+num_points = 10**7
 generators = 1000
 add_bead_annotations = False #Adding these annotations significantly increases compute time. (on the order of 10 seconds vs 30 minutes)
+panel_serve = False
 G = AtomGroup(generators, 1.1)
 seed = G.fixed_points((0,1))[0]
 df = G.coloured_limit_set_fast(num_points, seed=seed)
-scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour'])\
-            .opts(marker = "dot", size = 1,  color = 'black', width=1600, height=1600, data_aspect=1)
+print("    atom.py has finished computing the limit set")
+scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour']).opts(marker = "dot", size = 1, color = 'black')
 
 if add_bead_annotations:
+    print("    atom.py is computing beads")
     # circles which we are inverting across
     beads = [hv.Ellipse(float(centre.real), float(centre.imag), float(radius)*2).opts(color='red') for (centre, radius) in G.circles1]
     beads += [hv.Ellipse(float(centre.real), float(centre.imag), float(radius)*2).opts(color='blue') for (centre, radius) in G.circles2]
 
     # isometric circles of generators
+    print("    atom.py is computing isocircles")
     isocircles = [G.isometric_circle(w) for w in G.free_cayley_graph_bfs(1)]
     ellipses = [hv.Ellipse(float(centre.real), float(centre.imag), float(radius)*2).opts(color='gray') for (centre, radius) in isocircles] #<-the final parameter of hv.Ellipse is diameter, not radius
     scatter = reduce(lambda a,b: a*b, ellipses+beads, scatter)
-    if panel_app:
-        pn.panel(scatter).servable(title=f'Accola\'s atom group')
-    else:
-        hv.save(scatter, "atom.png")
+    print("    atom.py has added beads and isometric circles")
+
+print("    atom.py is starting to shade")
+shaded = datashade(scatter).opts(width=2000, height=2000, data_aspect=1)
+if panel_serve:
+    print("    atom.py is starting to serve")
+    pn.serve(shaded, cmap="black")
 else:
-    if panel_app:
-        pn.panel(scatter).servable(title=f'Accola\'s atom group')
-    else:
-        hv.save(scatter, "atom_no_beads.png")
+    print("    atom.py is starting to save image")
+    hv.save(shaded, "atom.png" if add_bead_annotations else "atom_no_beads.png")
+
