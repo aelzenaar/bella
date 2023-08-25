4ccc01121a10febb67e7af3671da423b08c4e711
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2023-08-26 10:35:11 +1200

    Update Zarrow example

--- examples/zarrow.py
+++ examples/zarrow.py
@@ -19,21 +19,27 @@ class ZarrowGroup(cayley.GroupCache):
         super().__init__([self.S,self.T])
 
 # Just compute the gasket as the limit set of the μ=2i parabolic Riley group, and save it to an image file.
-num_points = 10**5
+num_points = 10**6
 G = ZarrowGroup()
+
 seed = G.fixed_points((0,1))[0]
-window_radius = 100
-x_range = (float(seed.real)-window_radius,float(seed.real)+window_radius)
-y_range = (float(seed.imag)-window_radius,float(seed.imag)+window_radius)
 df = G.coloured_limit_set_fast(num_points, seed=seed)
 scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour'])\
-            .opts(marker = "dot", size = 0.1,  color = 'colour', width=1600, height=1600, data_aspect=1, cmap='Set1')\
-              .redim(x=hv.Dimension('x', range=x_range),y=hv.Dimension('y', range=y_range))
-
+            .opts(marker = "dot", size = 0.1,  color = 'colour', frame_width=800, frame_height=800, data_aspect=1, cmap='Set1')\
+              .redim(x=hv.Dimension('x', range=(-200,200)),y=hv.Dimension('y', range=(-200,200)))
 
 #isometric circles of generators
 isocircles = [G.isometric_circle(w) for w in G.free_cayley_graph_bfs(1)]
 ellipses = [hv.Ellipse(float(centre.real), float(centre.imag), float(radius)*2).opts(color='gray') for (centre, radius) in isocircles] #<-the final parameter of hv.Ellipse is diameter, not radius
 scatter = reduce(lambda a,b: a*b, ellipses, scatter)
 
-hv.save(scatter, 'zarrow.png', fmt='png')
+hv.save(scatter, 'zarrow1.png', fmt='png')
+print(f'    zarrow.py (1/2): drew images of {mp.nstr(seed)}')
+
+seed = 100j
+df = G.coloured_limit_set_fast(num_points, seed=seed)
+scatter = hv.Scatter(df, kdims = ['x'], vdims = ['y','colour'])\
+            .opts(marker = "dot", size = 0.1,  color = 'colour', frame_width=800, frame_height=800, data_aspect=1, cmap='Set1')\
+              .redim(x=hv.Dimension('x', range=(-200,200)),y=hv.Dimension('y', range=(-200,200)))
+hv.save(scatter, 'zarrow2.png', fmt='png')
+print(f'    zarrow.py (2/2): drew images of {mp.nstr(seed)}')
